afb8e63e235e657093b6275819a4c7dedf39f05b
Leandro Souza <leandro.souzza@outlook.com.br>
2022-05-05 17:01:16 -0300

    Criação dos botões de cálculo

--- Calculadora.interface/FormPrincipal.Designer.cs
+++ Calculadora.interface/FormPrincipal.Designer.cs
@@ -32,6 +32,9 @@
             this.BtnMais = new System.Windows.Forms.Button();
             this.LblValorCalcular = new System.Windows.Forms.Label();
             this.BtnIgual = new System.Windows.Forms.Button();
+            this.BtnMenos = new System.Windows.Forms.Button();
+            this.BtnVezez = new System.Windows.Forms.Button();
+            this.BtnDivide = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TxtValorCalcular
@@ -46,9 +49,9 @@
             // BtnMais
             // 
             this.BtnMais.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnMais.Location = new System.Drawing.Point(12, 98);
+            this.BtnMais.Location = new System.Drawing.Point(228, 365);
             this.BtnMais.Name = "BtnMais";
-            this.BtnMais.Size = new System.Drawing.Size(102, 99);
+            this.BtnMais.Size = new System.Drawing.Size(90, 62);
             this.BtnMais.TabIndex = 1;
             this.BtnMais.Text = "+";
             this.BtnMais.UseVisualStyleBackColor = true;
@@ -67,19 +70,53 @@
             // BtnIgual
             // 
             this.BtnIgual.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnIgual.Location = new System.Drawing.Point(216, 393);
+            this.BtnIgual.Location = new System.Drawing.Point(228, 433);
             this.BtnIgual.Name = "BtnIgual";
-            this.BtnIgual.Size = new System.Drawing.Size(102, 99);
+            this.BtnIgual.Size = new System.Drawing.Size(90, 59);
             this.BtnIgual.TabIndex = 3;
             this.BtnIgual.Text = "=";
             this.BtnIgual.UseVisualStyleBackColor = true;
             this.BtnIgual.Click += new System.EventHandler(this.BtnIgual_Click);
+            // 
+            // BtnMenos
+            // 
+            this.BtnMenos.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnMenos.Location = new System.Drawing.Point(228, 297);
+            this.BtnMenos.Name = "BtnMenos";
+            this.BtnMenos.Size = new System.Drawing.Size(90, 62);
+            this.BtnMenos.TabIndex = 4;
+            this.BtnMenos.Text = "-";
+            this.BtnMenos.UseVisualStyleBackColor = true;
+            this.BtnMenos.Click += new System.EventHandler(this.BtnMenos_Click);
+            // 
+            // BtnVezez
+            // 
+            this.BtnVezez.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnVezez.Location = new System.Drawing.Point(228, 232);
+            this.BtnVezez.Name = "BtnVezez";
+            this.BtnVezez.Size = new System.Drawing.Size(90, 59);
+            this.BtnVezez.TabIndex = 5;
+            this.BtnVezez.Text = "x";
+            this.BtnVezez.UseVisualStyleBackColor = true;
+            // 
+            // BtnDivide
+            // 
+            this.BtnDivide.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnDivide.Location = new System.Drawing.Point(228, 165);
+            this.BtnDivide.Name = "BtnDivide";
+            this.BtnDivide.Size = new System.Drawing.Size(90, 61);
+            this.BtnDivide.TabIndex = 6;
+            this.BtnDivide.Text = "/";
+            this.BtnDivide.UseVisualStyleBackColor = true;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(330, 504);
+            this.Controls.Add(this.BtnDivide);
+            this.Controls.Add(this.BtnVezez);
+            this.Controls.Add(this.BtnMenos);
             this.Controls.Add(this.BtnIgual);
             this.Controls.Add(this.LblValorCalcular);
             this.Controls.Add(this.BtnMais);
@@ -97,5 +134,8 @@
     private Button BtnMais;
     private Label LblValorCalcular;
     private Button BtnIgual;
-}
+        private Button BtnMenos;
+        private Button BtnVezez;
+        private Button BtnDivide;
+    }
 }
--- Calculadora.interface/FormPrincipal.cs
+++ Calculadora.interface/FormPrincipal.cs
@@ -5,29 +5,34 @@
         private decimal ValorCalcular;
         private bool EhSoma, EhMultiplicacao, EhDivisao, EhSubtracao;
 
+        private void BtnMenos_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void BtnIgual_Click(object sender, EventArgs e)
         {
             if (EhSoma)
             {
-                decimal ValorTotal = ValorCalcular + decimal.Parse (TxtValorCalcular.Text);
+                decimal ValorTotal = ValorCalcular + decimal.Parse(TxtValorCalcular.Text);
 
                 LblValorCalcular.Text = $"{LblValorCalcular.Text} {TxtValorCalcular.Text} =";
 
                 TxtValorCalcular.Text = ValorTotal.ToString();
+
             }
         }
+            public FormPrincipal()
+            {
+                InitializeComponent();
+            }
 
-        public FormPrincipal()
-        {
-            InitializeComponent();
-        }
+            private void BtnMais_Click(object sender, EventArgs e)
+            {
+                EhSoma = true;
+                ValorCalcular = Decimal.Parse(TxtValorCalcular.Text);
 
-        private void BtnMais_Click(object sender, EventArgs e)
-        {
-            EhSoma = true;
-            ValorCalcular = Decimal.Parse(TxtValorCalcular.Text);
-
-            LblValorCalcular.Text = $"{ValorCalcular} +";
+                LblValorCalcular.Text = $"{ValorCalcular} +";
+            }
         }
     }
-}
